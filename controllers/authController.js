b5edd0127f0212a6af796f4f4946478925fa2c21
Suman Karmakar <sumankarmakar@Sumans-MacBook-Air.local>
2022-06-17 01:38:01 +0530

    added logging out feature

--- controllers/authController.js
+++ controllers/authController.js
@@ -77,3 +77,10 @@ module.exports.login_post = async (req, res) => {
         res.status(400).json({ errors });
     }
 }
+
+//logging users out
+
+module.exports.logout_get = (req, res) => {
+    res.cookie('jwt', '', { maxAge: 1 });
+    res.redirect('/');
+}
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,6 +8,7 @@ router.get('/register', authController.register_get);
 router.post('/register', authController.register_post);
 router.get('/login', authController.login_get);
 router.post('/login', authController.login_post);
+router.get('/logout', authController.logout_get); //logging out user
 router.get('/mappage', requireAuth, authController.mappage_get); //added routing for mappage
 
 
