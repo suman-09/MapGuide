f72ea1d203d20c14fa96686322bb3b8268888b45
Suman Karmakar <sumankarmakar@Sumans-MacBook-Air.local>
2022-08-17 19:33:19 +0530

    changed the UI of mappage

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
+const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const authRoutes = require('./routes/authRoutes');
 const cookieParser = require('cookie-parser');
+const server = require('http').createServer(app);
 
 //load env vars
 dotenv.config({path: './.env'});
-
-const app = express();
 
 //middleware
 app.use(express.static("public"));
@@ -26,31 +26,7 @@ mongoose.connect(dbURI)
 
 //routes
 app.get('/', (req,res) => {
-    res.render('home.ejs', {name: 'kyle'}); //changed from index to home.ejs
+    res.render('home.ejs', {name: 'kyle'});
 });
 app.use(authRoutes);
-
-//only for learning
-
-// //cookies
-// app.get('/set-cookies', (req, res) => {
-
-//     res.cookie('newUser', false);
-//     res.cookie('isEmployee', true, { maxAge: 1000 * 60 * 60 *24, httpOnly: true }); 
-    
-//     //secure: true now this cookie is only gonna send when there will be an secure connection like https in our case its not going to send as we r in http 
-//     //httpOnly: true this cookie is not accessable by frontend js
-//     //both the properties are important for auth
-
-//     res.send('you got the cookies!');
-
-// });
-// app.get('/read-cookies', (req, res) => {
-
-//     const cookies = req.cookies;
-//     console.log(cookies.newUser);
-
-//     res.json(cookies);
-
-// });
 
